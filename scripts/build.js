c3155ea0bb39129d3f3de388119048589a6252dc
Karim Alibhai <karim@foko.co>
2018-03-15 02:58:57 -0400

    Add: README compiling from module list

---
+++ scripts/build.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+/**
+ * @file scripts/build.js
+ * @copyright 2018-present Karim Alibhai. All rights reserved.
+ */
+
+const fs = require('fs')
+const path = require('path')
+const dot = require('dot')
+
+Object.assign(dot.templateSettings, {
+  strip: false,
+})
+
+const src = fs.readFileSync(path.resolve(__dirname, '..', 'README-tpl.md'), 'utf8')
+const tpl = dot.template(src)
+
+const srcdir = path.resolve(__dirname, '..', 'src')
+const modules = fs.readdirSync(srcdir)
+  .filter(mod => mod !== 'frenzie' && fs.statSync(`${srcdir}/${mod}`).isDirectory())
+
+const out = tpl({
+  modules: modules.map(mod => ` - [${mod}](src/${mod})`),
+})
+
+fs.writeFileSync(path.resolve(__dirname, '..', 'README.md'), out)
