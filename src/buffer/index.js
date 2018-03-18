1a9fa22b9150af3cb84c62141c85e72cdc9d7224
Karim Alibhai <karim@foko.co>
2018-03-18 19:20:38 -0400

    Add: forcibly sensitive data in allocUnsafe

--- src/buffer/index.js
+++ src/buffer/index.js
@@ -3,8 +3,43 @@
  * @copyright 2018-present Foko Inc. All rights reserved.
  */
 
-export default function factory(buffer) {
-  // ...
+import { shouldError } from '../frenzie/utils'
+
+const RANDOM_DATA = 'PASSWORD'
+
+export default function factory(buffer, options = {}) {
+  const { Buffer } = buffer
+  const {
+    allocUnsafe,
+    allocUnsafeSlow,
+  } = Buffer
+
+  function createBuffer(size) {
+    const data = RANDOM_DATA.repeat( Math.ceil(size / RANDOM_DATA.length) )
+    return data.substr(0, size)
+  }
+
+  function allocRandomly(method) {
+    return function (size) {
+      if (
+        typeof size === 'number' &&
+        size > 0 &&
+        shouldError(options.threshold)
+      ) {
+        return Buffer.from(createBuffer(size), 'utf8')
+      }
+
+      return method.call(Buffer, size)
+    }
+  }
+
+  if (typeof allocUnsafe === 'function') {
+    Buffer.allocUnsafe = allocRandomly(allocUnsafe)
+  }
+
+  if (typeof allocUnsafeSlow === 'function') {
+    Buffer.allocUnsafeSlow = allocRandomly(allocUnsafeSlow)
+  }
 
   return buffer
 }
