1db652b7737da8b15a32e12e6027da15c075783e
Karim Alibhai <karim@foko.co>
2018-03-31 21:09:43 -0400

    Remove: extra files

--- req.js
+++
@@ -1,8 +0,0 @@
-require('request')({
-  url: 'http://www.google.com/',
-}, (err, res, body) => console.log(JSON.stringify({
-  err: String(err),
-  resExists: !!res,
-  status: (res || {}).statusCode,
-  bodyExists: !!body,
-}, null, 2)))
